733137413ab34c9a84283cf668f7fa9b9660f629
c <jscc2333@163.com>
2017-07-22 08:55:36 +0800

    完成通用数据库连接类

---
+++ src/com/tlb/beans/UtilConnection.java
@@ -0,0 +1,17 @@
+package com.tlb.beans;
+
+import java.sql.*;
+
+public class UtilConnection {
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String dburl = "jdbc:mysql://localhost:3306/flowershop";
+			conn = DriverManager.getConnection(dburl, "root", "302434");
+		} catch (Exception e) {
+			return null;
+		}
+		return conn;
+	}
+}
